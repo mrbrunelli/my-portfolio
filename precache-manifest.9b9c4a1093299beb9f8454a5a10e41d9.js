ec78a16c5233694b5486047bfb09c005bf064b1f
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-06-19 15:27:58 -0300

    Updates

---
+++ precache-manifest.9b9c4a1093299beb9f8454a5a10e41d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0216de9df56e5df9f5bcb899d2320f5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "510d972f21cb7bdea538",
+    "url": "/my-portfolio/static/css/2.54fec870.chunk.css"
+  },
+  {
+    "revision": "e9cc9169eafb571e7aea",
+    "url": "/my-portfolio/static/css/main.065906df.chunk.css"
+  },
+  {
+    "revision": "510d972f21cb7bdea538",
+    "url": "/my-portfolio/static/js/2.d31196b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-portfolio/static/js/2.d31196b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9cc9169eafb571e7aea",
+    "url": "/my-portfolio/static/js/main.ba6f6b72.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mrbrunelli/my-portfolio/precache-manifest.f1d0c875108c761ba31c2cdd7b5bde08.js"
+  "/my-portfolio/precache-manifest.9b9c4a1093299beb9f8454a5a10e41d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrbrunelli/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
