55aec4914eee574ca539d72e7c1d29b57f98a424
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-06-22 12:31:14 -0300

    add database json for Education timeline

---
+++ src/data/Education/courses.ts
@@ -0,0 +1,106 @@
+const courses = [
+    {
+        title: 'Sistemas Para Internet',
+        institution: 'Faculdade Alfa',
+        location: 'Umuarama, PR',
+        description: 'Webdesign e Dispositivos Móveis',
+        technologies: [
+            'PHP',
+            'Java',
+            'JavaScrip',
+            'Banco de Dados',
+            'Engenharia de Software',
+        ]
+    },
+    {
+        title: 'Next Level Week #1',
+        institution: 'Rocketseat',
+        location: '',
+        description: 'Desenvolvimento do Ecoleta, aplicação para ajudar pessoas a encontrarem pontos de coleta para reciclagem',
+        technologies: [
+            'Node.js',
+            'ReactJS',
+            'React Native',
+            'SQlite',
+            'Engenharia de Software',
+        ]
+    },
+    {
+        title: 'RegExp: Expressões Regulares',
+        institution: 'Glider Cursos',
+        location: '',
+        description: 'Desenvolvimento de Expressões Regulares para manipular padrões de texto',
+        technologies: [
+            'RegExp'
+        ]
+    },
+    {
+        title: 'Docker: Ferramenta essencial para Desenvolvedores',
+        institution: 'Cod3r Cursos',
+        location: '',
+        description: 'Criar aplicações conteinerizadas, imagens personalizadas e soluções que rodem da mesma forma em qualquer ambiente',
+        technologies: [
+            'Docker',
+            'Docker Hub',
+            'Docker Compose',
+        ]
+    },
+    {
+        title: 'Semana Omnistack 11.0',
+        institution: 'Rocketseat',
+        location: '',
+        description: 'Desenvolvimento do Be The Hero, aplicação para ajudar ONGs a cadastrar demandas e receber contribuições',
+        technologies: [
+            'Node.js',
+            'ReactJS',
+            'React Native',
+            'SQlite',
+        ]
+    },
+    {
+        title: 'UI/UX Adobe XD',
+        institution: 'Origamid',
+        location: '',
+        description: 'Desenvolvimento de interfaces de usuário',
+        technologies: [
+            'Adobe XD',
+        ]
+    },
+    {
+        title: 'JavaScript ES6',
+        institution: 'Skylab, Rocketseat',
+        location: '',
+        description: 'Curso de JavaScript ES6',
+        technologies: [
+            'Arrow Functions',
+            'Class ',
+            'Arrays Methods',
+            'Promises',
+        ]
+    },
+    {
+        title: 'Qlik Sense',
+        institution: 'André Prado, Udemy',
+        location: '',
+        description: 'Levantamento de projetos de Business Intelligence, Extração de dados, Aplicações visuais',
+        technologies: [
+            'KPIs',
+            'Gráficos',
+            'Análise Histórica',
+        ]
+    },
+    {
+        title: 'Banco de Dados MySQL',
+        institution: 'Curso em Vídeo',
+        location: '',
+        description: 'Conhecendo mais sobre a linguagem SQL',
+        technologies: [
+            'Instruções',
+            'Cláusulas',
+            'Agrupadores',
+            'Subconsultas',
+        ]
+    },
+]
+
+export default courses
--- src/pages/Education/index.tsx
+++ src/pages/Education/index.tsx
@@ -4,6 +4,7 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css'
 import { FiArrowLeft, FiArrowRight, FiMapPin } from 'react-icons/fi'
 import { FaJs, FaDatabase, FaDocker, FaAsterisk, FaAdobe, FaGraduationCap, FaUniversity, FaChartLine } from 'react-icons/fa'
+import courses from '../../data/Education/courses'
 import './styles.css'
 
 const Education = () => {
@@ -11,140 +12,23 @@ const Education = () => {
         <>
             <div className="div-scroll">
                 <VerticalTimeline>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="2019 - present"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaUniversity />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Sistemas Para Internet</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Faculdade Alfa</h4>
-                        <h5 className="vertical-timeline-element-subtitle"><FiMapPin /> Umuarama, PR</h5>
-                        <p>- Webdesign e Dispositivos Móveis</p>
-                        <p>- PHP | Java | JavaScript | Banco de dados | Engenharia de Software</p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="06/2020"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaJs />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Next Level Week #1</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Rocketseat</h4>
-                        <p>- Desenvolvimento do Ecoleta, aplicação para ajudar pessoas a encontrarem pontos de coleta para reciclagem</p>
-                        <p>- Node.js | ReactJS | React Native | SQlite</p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="04/2020"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaAsterisk />}
-                    >
-                        <h3 className="vertical-timeline-element-title">RegExp (Expressões Regulares)</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Glider Cursos</h4>
-                        <p>- Desenvolvimento de Expressões Regulares para manipular padrões de texto</p>
-                        <p>- RegExp</p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="03/2020"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaDocker />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Docker: Ferramenta essencial para Desenvolvedores</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Cod3r Cursos</h4>
-                        <p>- Criar aplicações conteinerizadas, imagens personalizadas e soluções que rodem da mesma forma em qualquer ambiente</p>
-                        <p>- Docker | Docker Hub | Docker Compose</p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="03/2020"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaJs />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Semana Omnistack 11.0</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Rocketseat</h4>
-                        <p>- Desenvolvimento do Be The Hero, aplicação para ajudar ONGs a cadastrar demandas e receber contribuições</p>
-                        <p>- Node.js | ReactJS | React Native | SQlite</p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="02/2020"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaAdobe />}
-                    >
-                        <h3 className="vertical-timeline-element-title">UI/UX Adobe XD</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Origamid</h4>
-                        <p>- Desenvolvimento de interfaces de usuário</p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="01/2020"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaJs />}
-                    >
-                        <h3 className="vertical-timeline-element-title">JavaScript ES6</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Skylab, Rocketseat</h4>
-                        <p>- Curso de JavaScript</p>
-                        <p>- Arrow Functions | Class | Arrays Methods | Promises</p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="12/2019"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaChartLine />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Qlik Sense</h3>
-                        <h4 className="vertical-timeline-element-subtitle">André Prado, Udemy</h4>
-                        <p>- Levantamento de projetos de Business Intelligence, Extração de dados, Aplicações visuais</p>
-                        <p>- KPIs | Gráficos | Análise Histórica</p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        contentStyle={{ background: '#8844ee', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
-                        date="07/2019"
-                        dateClassName="text-light"
-                        iconStyle={{ background: '#8844ee', color: '#fff' }}
-                        icon={<FaDatabase />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Banco de Dados MySQL</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Curso em Vídeo</h4>
-                        <p>- Conhecendo mais sobre a linguagem SQL</p>
-                        <p>- Instruções | Cláusulas | Agrupadores | Subconsultas</p>
-                    </VerticalTimelineElement>
+                    {courses.map(course => (
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            contentStyle={{ background: '#8844ee', color: '#fff' }}
+                            contentArrowStyle={{ borderRight: '7px solid  #8844ee' }}
+                            date="2019 - present"
+                            dateClassName="text-light"
+                            iconStyle={{ background: '#8844ee', color: '#fff' }}
+                            icon={<FaUniversity />}
+                        >
+                            <h3 className="vertical-timeline-element-title">{course.title}</h3>
+                            <h4 className="vertical-timeline-element-subtitle">{course.institution}</h4>
+                            <h5 className="vertical-timeline-element-subtitle">{course.location ? <FiMapPin /> : ''} {course.location}</h5>
+                            <p>- {course.description}</p>
+                            <p>- {course.technologies.join(' | ')}</p>
+                        </VerticalTimelineElement>
+                    ))}
                 </VerticalTimeline>
             </div>
             <div className="pagination">
