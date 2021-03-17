35c5d3f9cc67b3f4acc55a9582db35a425f9a154
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-03-17 18:31:46 -0300

    feat: update my jobs descriptions

--- src/data/Home/jobs.ts
+++ src/data/Home/jobs.ts
@@ -12,17 +12,17 @@ interface Jobs {
 
 const jobs: Jobs[] = [
     {
-        title: 'Desenvolvedor de Software',
+        title: 'Programador',
         company: 'Grupo Gazin',
         location: 'Douradina, PR',
-        description: 'Desenvolvimento de APIs, integrações, rotinas, microsserviços, sistemas web e desktop.',
+        description: 'Atendo demandas de incidentes e desenvolvo melhorias em sistemas internos utilizando várias tecnologias e linguagens de programação, focando principalmente em qualidade de software.',
         date: '2021 - present',
         icon: FiCode,
-    },{
+    }, {
         title: 'Analista de Sistemas',
         company: 'Grupo Gazin',
         location: 'Douradina, PR',
-        description: 'Análises, extrações de dados PostgreSQL, desenvolvimento de B.I com Qlik Sense, rotinas e cron jobs.',
+        description: 'Desenvolvi algoritmos que reduziram em 100% o lançamento manual de notas fiscais e ocorrências em plataformas de terceiros. Também fui responsável por fazer integrações REST com sistemas terceiros, criação de indicadores, extração de dados e desenvolvimento de soluções internas para o setor de Logística.',
         date: '2019 - 2021',
         icon: FiCode,
     },
